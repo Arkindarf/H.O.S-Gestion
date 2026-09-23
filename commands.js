35f687649733a10e9ddfb42dce92b6b381216f72
Arkindarf <arkindarf@gmail.com>
2026-09-23 16:57:06 +0200

    Modif 2

--- commands.js
+++ commands.js
@@ -1,23 +1,6 @@
 import 'dotenv/config';
 import {InstallGlobalCommands } from './utils.js';
 
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
-
-const SPECIAL_COMMAND = {
-  name: 'special',
-  description: 'Elle arrivera',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
-
 const ALL_COMMANDS = [];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
